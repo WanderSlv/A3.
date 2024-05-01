c0b40061b1c1a19ce57846b8b2d923914b4d4503
xilin <xilin@DESKTOP-OUKF3HO.bbrouter>
2024-05-01 14:05:40 -0300

    teste

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,9 @@
+package principal;
+import visao.FrmMenuPrincipal;
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmMenuPrincipal objeto = new FrmMenuPrincipal();
+        objeto.setVisible(true);
+    }
+}
